0d0572dd019a24472e68de0df66a6f233abc8c80
Pranavvk1 <145404092+Pranavvk1@users.noreply.github.com>
2025-02-21 13:11:31 -0500

    Update Move.java

--- src/Move.java
+++ src/Move.java
@@ -8,10 +8,6 @@ class Cube{
     {"Y", "Y", "Y", "Y"},
   };
 
-  // private int[] R = {0, 4, 5, 2};
-  // private int[] F = {0, 3, 5, 1};
-
-
   public void revolveRight(int face) {
     String temp = pieces[face][3];
     for (int i = 3; i > 0; i--) {
@@ -25,41 +21,48 @@ class Cube{
     temp[0] = pieces[faces[9]][faces[10]];
     temp[1] = pieces[faces[9]][faces[11]];
     for(int i = 9; i > 0; i -= 3){
-      pieces[faces[i]][faces[i + 1]] = pieces[faces[i - 3]][faces[i + 1]];
-      pieces[faces[i]][faces[i + 2]] = pieces[faces[i - 3]][faces[i + 2]];
+      pieces[faces[i]][faces[i + 1]] = pieces[faces[i - 3]][faces[i - 2]];
+      pieces[faces[i]][faces[i + 2]] = pieces[faces[i - 3]][faces[i - 1]];
     }
     pieces[faces[0]][faces[1]] = temp[0];
     pieces[faces[0]][faces[2]] = temp[1];
   }
+  
+  public void move(int face, int[] faces){
+      revolveRight(face);
+      revolveTwo(faces);
+  }
 
   public void R() {
-    revolveRight(3);
     int[] faces = {0, 1, 2, 4, 3, 0, 5, 1, 2, 2, 1, 2};
-    revolveTwo(faces);
+    move(3, faces);
   }
 
   public void L() {
-    revolveRight(1);
     int[] faces = {2, 0, 3, 5, 0, 3, 4, 1, 2, 0, 0, 3};
-    revolveTwo(faces);
+    move(1, faces)
   }
 
   public void U() {
-    revolveRight(0);
     int[] faces = {4, 0, 1, 3, 0, 1, 2, 0, 1, 1, 0, 1};
-    revolveTwo(faces);
+    move(0, faces);
   }
 
   public void D() {
-    revolveRight(5);
-    int[] faces = {1, 0, 1, 2, 0, 1, 3, 0, 1, 4, 0, 1};
-    revolveTwo(faces);
+    int[] faces = {1, 2, 3, 2, 2, 3, 3, 2, 3, 4, 2, 3};
+    move(5, faces);
   }
 
   public void F() {
-    revolveRight(2);
     int[] faces = {0, 2, 3, 3, 0, 3, 5, 0, 1, 1, 1, 2};
+    move(2, faces);
   }
+  
+  public void B() {
+    int[] faces = {1, 0, 3, 5, 3, 2, 3, 2, 1, 0, 1, 0};
+    move(4, faces);
+  }
+  
 
   public void printCube() {
     for(int i = 0; i < 6; i++) {
